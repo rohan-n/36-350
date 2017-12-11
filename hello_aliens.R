e5e0021c0075b56396aad495e014de6c82a07e7e
Rohan Nagalkar <rohan.nagalkar13@gmail.com>
2017-12-10 20:01:22 -0500

    For the final project

---
+++ hello_aliens.R
@@ -0,0 +1,6 @@
+cat("To whom it may concern; we are the proud people of Earth, and we seek to thrive, enrich our knowledge, and maintain physical dominance over the surrounding biodiversity", 
+    "Our journey to become a civilized society has been tumultuous, and we have a history rich and extensive (at least to us and our tiny brains).", 
+    "However, this may not be the case with you: maybe you have large brains and can process things faster than us - maybe a year to us is a second to you.", 
+    "Wait - are you smarter than us? You're a threat. Perhaps you should be eliminated. You see, we have very small egos and can't afford to be seen as the weaker species.",
+    "We are terribly sorry to have to do this to you. Maybe if you were interesting like the aliens from the movie Arrival we could have avoided this altogether - at least they look cool.",
+    sep = "\n")
